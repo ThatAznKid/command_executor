5d767ec9afc539aeaeabbad45ff618c2bad5c35a
achencoms <achencoms@gmail.com>
2016-11-18 11:47:03 -0500

    Add files via upload

--- ask.c
+++ ask.c
@@ -7,18 +7,16 @@ int main(int argc, char *argv[]){
 
 	printf("What would you like to do Mister DDubleU?: ");
 	char line[100];
-	scanf("%s",line);
+	fgets(line,sizeof(line),stdin);
 	char * s = line;
 	char * command[5];
 	int i = 0;
 	while(s){
-		printf("%s\n",s);
 		command[i] = strsep(&s," ");
+		printf("%s\n",command[i]);
 		i++;
-		printf("%d\n",i);
 	}
-	printf("%s\n",command[1]);
 	command[i] = 0;
-	//execvp(command[0],command);	
+	execvp(command[0],command);
 
 }
