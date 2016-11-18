c65c1c8f60819e2322f14a0cee8a96c3ae30fbc9
whereami <jiaweichen29@gmail.com>
2016-11-18 23:38:38 +0000

    seems all good!

--- ask.c
+++ ask.c
@@ -7,8 +7,8 @@ int main(int argc, char *argv[]){
 
 	printf("What would you like to do Mister DDubleU?:");
 	char line[100];
-	fgets(line,sizeof(line),stdin);
-	line[strlen(line) - 1] = '\0';
+	fgets(line,sizeof(line),stdin); 
+	line[strlen(line) - 1] = '\0'; //gets rid of the new line
 	char * s = line;
 	char * command[5];
 	int i = 0;
