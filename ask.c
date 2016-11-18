ddaef68d864a00cb5b9b156dc6663e8fc9541274
achencoms <achencoms@gmail.com>
2016-11-18 14:49:50 -0500

    Add files via upload
    I FIXED IT

--- ask.c
+++ ask.c
@@ -5,18 +5,17 @@
 	
 int main(int argc, char *argv[]){
 
-	printf("What would you like to do Mister DDubleU?: ");
+	printf("What would you like to do Mister DDubleU?:");
 	char line[100];
 	fgets(line,sizeof(line),stdin);
+	line[strlen(line) - 1] = '\0';
 	char * s = line;
 	char * command[5];
 	int i = 0;
 	while(s){
 		command[i] = strsep(&s," ");
-		printf("%s\n",command[i]);
 		i++;
 	}
-	command[i] = 0;
+	command[i] = NULL;
 	execvp(command[0],command);
-
 }
